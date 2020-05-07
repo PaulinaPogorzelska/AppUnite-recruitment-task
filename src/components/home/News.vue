7dae4a92dacce7bdf1769d7b894c64d574cede1e
Paulina <papogorzelska@gmail.com>
2020-05-07 18:46:13 +0200

    Early stage of Film list

---
+++ src/components/home/News.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NewsItem
+      v-for="(singleNews, key) in getNews"
+      :key="key"
+      :singleNews="singleNews"
+    ></NewsItem>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import NewsItem from "./NewsItem.vue";
+export default {
+  computed: {
+    ...mapGetters(["getNews"])
+  },
+  components: {
+    NewsItem
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
-  <Filters></Filters>
+  <div>
+    <Filters></Filters>
+    <News></News>
+  </div>
 </template>
 
 <script>
 import Filters from "@/components/home/Filters.vue";
+import News from "@/components/home/News.vue";
 export default {
   components: {
-    Filters
+    Filters,
+    News
   }
 };
 </script>
